95a25fc659a95ceed8677673af660ee69bab7a2c
rahulbakkatatla <rahukbhaktala@gmail.com>
2026-01-05 14:16:55 +0530

    chore: add Spring Boot project structure (Week 3 Day 1)

---
+++ src/main/java/com/salescrm/salescrm/SalescrmApplication.java
@@ -0,0 +1,12 @@
+package com.salescrm.salescrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalescrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalescrmApplication.class, args);
+    }
+}
---
+++ src/test/java/com/salescrm/salescrm/SalescrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salescrm.salescrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalescrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
