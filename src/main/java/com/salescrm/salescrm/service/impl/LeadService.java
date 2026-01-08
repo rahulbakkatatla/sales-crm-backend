eb3814cc3e62984ebf2f95bc19cf25809ade6c62
rahulbakkatatla <rahukbhaktala@gmail.com>
2026-01-08 16:17:46 +0530

    feat: enforce lead status lifecycle and valid state transitions

--- src/main/java/com/salescrm/salescrm/service/impl/LeadService.java
+++ src/main/java/com/salescrm/salescrm/service/impl/LeadService.java
@@ -20,9 +20,9 @@ public class LeadService {
     }
 
     public Lead getLeadById(Long leadId, String requester) {
-        // fake lead for now
         Lead lead = new Lead();
         lead.setOwner("rahul");
+        lead.setStatus(LeadStatus.NEW); // ⭐ ADD THIS
 
         if (!lead.getOwner().equals(requester)) {
             throw new IllegalArgumentException("Access denied");
@@ -30,5 +30,53 @@ public class LeadService {
 
         return lead;
     }
+
+
+    public Lead updateLeadStatus(Long leadId, LeadStatus newStatus, String requester){
+        Lead lead = getLeadById(leadId, requester);
+
+        if (lead.getStatus() == LeadStatus.CONVERTED ||
+                lead.getStatus() == LeadStatus.LOST) {
+
+            throw new IllegalArgumentException(
+                    "Cannot update status of a converted or lost lead"
+            );
+        }
+
+        LeadStatus currentStatus = lead.getStatus();
+
+        switch (currentStatus) {
+
+            case NEW:
+                if (newStatus != LeadStatus.CONTACTED) {
+                    throw new IllegalArgumentException("Invalid status transition");
+                }
+                break;
+
+            case CONTACTED:
+                if (newStatus != LeadStatus.QUALIFIED) {
+                    throw new IllegalArgumentException("Invalid status transition");
+                }
+                break;
+
+            case QUALIFIED:
+                if (newStatus != LeadStatus.CONVERTED &&
+                        newStatus != LeadStatus.LOST) {
+                    throw new IllegalArgumentException("Invalid status transition");
+                }
+                break;
+
+            default:
+                throw new IllegalArgumentException("Invalid status transition");
+        }
+
+        lead.setStatus(newStatus);
+        return lead;
+
+
+
+    }
+
 }
+
 
